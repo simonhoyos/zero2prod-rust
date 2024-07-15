d22ead9e6765eac3bd13e7cad3d159e10f8f1851
Simon Hoyos <shmesa23@gmail.com>
2024-07-15 17:18:40 -0500

    feat: add telemetry
    Print better messages to the console with enriched data.
    These changes can be integrated with any OpenTelemetry-compatible
    service.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,8 +13,27 @@ pub struct FormData {
     name: String,
 }
 
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, db_pool),
+    fields(
+        subscriber_email = %form.email,
+        subscriber_name = %form.name,
+    )
+)]
 pub async fn subscribe(form: Form<FormData>, db_pool: Data<PgPool>) -> HttpResponse {
-    match sqlx::query!(
+    match insert_subscriber(&db_pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, db_pool)
+)]
+async fn insert_subscriber(db_pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
             INSERT INTO subscription (id, email, name, subscribed_at)
             VALUES ($1, $2, $3, $4)
@@ -24,13 +43,12 @@ pub async fn subscribe(form: Form<FormData>, db_pool: Data<PgPool>) -> HttpRespo
         form.name,
         Utc::now()
     )
-    .execute(db_pool.get_ref())
+    .execute(db_pool)
     .await
-    {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(e) => {
-            println!("Failed to execute query: {}", e);
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+
+    Ok(())
 }
